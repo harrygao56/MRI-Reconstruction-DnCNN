58c14804ab78d8e0e0f872b0efc8d18766894073
Harry Gao <89803634+harrygao56@users.noreply.github.com>
2022-03-11 12:52:47 -0600

    Add files via upload

---
+++ model.py
@@ -0,0 +1,25 @@
+import torch.nn as nn
+
+
+class DnCNN(nn.Module):
+    def __init__(self, channels=1, num_of_layers=7):
+        super(DnCNN, self).__init__()
+        kernel_size = 3
+        padding = 1
+        features = 16
+        layers = []
+        layers.append(nn.Conv2d(in_channels=channels, out_channels=features, kernel_size=kernel_size,
+                                padding=padding, bias=False))
+        layers.append(nn.ReLU(inplace=True))
+        for _ in range(num_of_layers-2):
+            layers.append(nn.Conv2d(in_channels=features, out_channels=features, kernel_size=kernel_size,
+                                    padding=padding, bias=True))
+            # layers.append(nn.BatchNorm2d(features))
+            layers.append(nn.ReLU(inplace=True))
+        layers.append(nn.Conv2d(in_channels=features, out_channels=channels, kernel_size=kernel_size,
+                                padding=padding, bias=False))
+        self.dncnn = nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = self.dncnn(x)
+        return out
